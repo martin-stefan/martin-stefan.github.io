6cf59365f06820f5c858850365eef5a019c7e6bb
Stefan Martin <stefanmartin410@gmail.com>
2020-07-09 19:20:52 -0400

    card setup

--- projects/projects.js
+++ projects/projects.js
@@ -4,17 +4,23 @@ $(document).ready(function () {
     url: "projects.json",
     dataType: "json",
     success: function(responseData, status){
-      var output = "<ul class='list'>";
+      var output = '<div class="row">';
+      // output += "<ul class='list'>";
       $.each(responseData.project, function(i, item) {
+        output += '<div class="col-1-of-2">';
         output += '<div class="card">';
-        output += '<li class="listelm">';
+        output += '<div class="card__side">';
+        // output += '<li class="listelm">';
         output += '<p class="smallTitle">' + item.title + '</p>';
         output += '<p class="subTitle">' + item.date + '</p>';
         output += '<p class="smallDesc">' + item.desc + '</p>';
-        output += '</li>';
+        // output += '</li>';
+        output += '</div>';
+        output += '</div>';
         output += '</div>';
       });
-      output += '</ul>';
+      // output += '</ul>';
+      output += '</div>';
       $('#projects').html(output);
     }
   });
