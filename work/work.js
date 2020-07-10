7b415e29e59330c6d1939146ec0cdccf7fa55fa7
Stefan Martin <stefanmartin410@gmail.com>
2020-07-09 22:01:02 -0400

    added a nav button

--- work/work.js
+++ work/work.js
@@ -4,17 +4,20 @@ $(document).ready(function () {
     url: "work.json",
     dataType: "json",
     success: function(responseData, status){
-      var output = "<ul class='list'>";
+      var output = '<div class="row">';
+      // var output = "<ul class='list'>";
       $.each(responseData.job, function(i, item) {
+        output += '<div class="col-1-of-2">';
         output += '<div class="card">';
-        output += '<li class="listelm">';
+        // output += '<li class="listelm">';
         output += '<p class="smallTitle">' + item.title + " - " + item.comp + '</p>';
         output += '<p class="subTitle">' + item.date + '</p>';
         output += '<p class="smallDesc">' + item.desc + '</p>';
-        output += '</li>';
+        // output += '</li>';
+        output += '</div>';
         output += '</div>';
       });
-      output += '</ul>';
+      // output += '</ul>';
       $('#jobs').html(output);
     }
   });
