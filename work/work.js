915ef2db7f360f7ce77aaf48ab54169918b2c22b
Stefan Martin <stefanmartin410@gmail.com>
2020-07-11 23:59:13 -0400

    updates

--- work/work.js
+++ work/work.js
@@ -7,8 +7,9 @@ $(document).ready(function () {
       var output = '<div class="row">';
       // var output = "<ul class='list'>";
       $.each(responseData.job, function(i, item) {
-        output += '<div class="col-1-of-2">';
+        output += '<div class="col-1-of-3">';
         output += '<div class="card">';
+        output += '<div class="card__side>';
         // output += '<li class="listelm">';
         output += '<p class="smallTitle">' + item.title + " - " + item.comp + '</p>';
         output += '<p class="subTitle">' + item.date + '</p>';
@@ -16,8 +17,10 @@ $(document).ready(function () {
         // output += '</li>';
         output += '</div>';
         output += '</div>';
+        output += '</div>';
       });
       // output += '</ul>';
+      output += '</div>';
       $('#jobs').html(output);
     }
   });
